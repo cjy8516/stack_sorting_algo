56d7f628b7520381548022c3b20175091e572431
cjy8516 <161845356+cjy8516@users.noreply.github.com>
2026-02-08 19:56:30 +0000

    Add files via upload

---
+++ stack_actions.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_actions.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinchen <jinchen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/31 11:01:15 by jinchen           #+#    #+#             */
+/*   Updated: 2026/02/07 21:22:11 by jinchen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "stack_actions.h"
+#include "ft_printf.h"
+#include "utils.h"
+#include "free.h"
+
+void	swap_stack(t_stack *stack, char *action)
+{
+	int	temp;
+
+	ft_printf("%s\n", action);
+	temp = stack->head->value;
+	stack->head->value = stack->head->next->value;
+	stack->head->next->value = temp;
+	temp = stack->head->index;
+	stack->head->index = stack->head->next->index;
+	stack->head->next->index = temp;
+}
+
+void	rotate_stack(t_stack *stack, char *action)
+{
+	if (stack->nodes <= 1)
+		return ;
+	ft_printf("%s\n", action);
+	stack->head = stack->head->next;
+	stack->tail = stack->tail->next;
+}
+
+void	reverse_rotate_stack(t_stack *stack, char *action)
+{
+	if (stack->nodes <= 1)
+		return ;
+	ft_printf("%s\n", action);
+	stack->head = stack->head->prev;
+	stack->tail = stack->tail->prev;
+}
+
+void	push_stack(t_stack *from, t_stack *to, char *action)
+{
+	t_lst	*new_lst;
+
+	new_lst = malloc(sizeof(t_lst));
+	if (!new_lst)
+		error_message("Error\n");
+	new_lst->value = from->head->value;
+	new_lst->index = from->head->index;
+	if (to->head == NULL)
+	{
+		new_lst->prev = NULL;
+		new_lst->next = NULL;
+		to->head = new_lst;
+		to->tail = new_lst;
+	}
+	else
+	{
+		new_lst->next = to->head;
+		new_lst->prev = to->tail;
+		to->head->prev = new_lst;
+		to->tail->next = new_lst;
+        to->head = new_lst;
+	}
+    to->nodes++;
+	push_stack_2(from, action);
+}
+
+void	push_stack_2(t_stack *from, char *action)
+{
+	if (from->nodes == 1)
+	{
+		free(from->head);
+		from->head = NULL;
+		from->tail = NULL;
+	}
+	else if (from->nodes == 2)
+	{
+		from->tail->next = NULL;
+		from->head->prev = NULL;
+		free(from->head);
+		from->head = from->tail;
+	}
+	else
+	{
+		from->head->next->prev = from->tail;
+		from->tail->next = from->head->next;
+		free(from->head);
+		from->head = from->tail->next;
+	}
+	--from->nodes;
+	ft_printf("%s\n", action);
+}
