b2eb0ab515975f91fdc7fdd8565e542c940dc71f
cjy8516 <161845356+cjy8516@users.noreply.github.com>
2026-02-08 19:57:02 +0000

    Add files via upload

---
+++ butterfly.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   butterfly.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinchen <jinchen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/25 14:16:16 by jinchen           #+#    #+#             */
+/*   Updated: 2026/02/01 17:46:32 by jinchen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "butterfly.h"
+#include "stack_actions.h"
+#include "utils.h"
+
+void	find_max_and_push_b(t_lst *lst, t_stack *st_a, int size)
+{
+	int	i;
+	int	is_tail;
+
+	i = 0;
+	is_tail = 0;
+	while (is_tail == 0)
+	{
+		if (lst->index == size - 1)
+		{
+			if (i > size - i - 1)
+			{
+				while (i++ != size)
+					reverse_rotate_stack(st_a, "rra");
+			}
+			else
+				while (i-- > 0)
+					rotate_stack(st_a, "ra");
+			break ;
+		}
+		++i;
+		lst = lst->next;
+		if (lst == st_a->head)
+			is_tail = 1;
+	}
+}
+
+void	find_max_and_push_a(t_lst *lst, t_stack *st_b, int size)
+{
+	int	i;
+	int	is_tail;
+
+	i = 0;
+	is_tail = 0;
+	while (is_tail == 0)
+	{
+		if (lst->index == size - 1)
+		{
+			if (i > size - i - 1)
+			{
+				while (i++ != size)
+					reverse_rotate_stack(st_b, "rrb");
+			}
+			else
+				while (i-- > 0)
+					rotate_stack(st_b, "rb");
+			break ;
+		}
+		++i;
+		lst = lst->next;
+		if (lst == st_b->head)
+			is_tail = 1;
+	}
+}
+
+void	sort_stack(t_stack *st_a, t_stack *st_b)
+{
+	t_lst	*lst;
+
+	while (st_b->head != NULL)
+	{
+		lst = st_b->head;
+		find_max_and_push_a(lst, st_b, st_b->nodes);
+		push_stack(st_b, st_a, "pa");
+	}
+}
+
+void	make_butterfly(t_stack *st_a, t_stack *st_b, int chunk)
+{
+	int		counter;
+	t_lst	*lst;
+
+	counter = 0;
+	lst = st_a->head;
+	while (st_a->head != NULL)
+	{
+		if (lst->index <= counter)
+		{
+			push_stack(st_a, st_b, "pb");
+			rotate_stack(st_b, "rb");
+			++counter;
+		}
+		else if (lst->index <= counter + chunk)
+		{
+			push_stack(st_a, st_b, "pb");
+			++counter;
+		}
+		else
+			rotate_stack(st_a, "ra");
+		lst = st_a->head;
+	}
+}
+
+void	butterfly_algo(t_stack *st_a, t_stack *st_b, int size)
+{
+	int	chunk;
+
+	chunk = generate_chunk(size);
+	make_butterfly(st_a, st_b, chunk);
+	sort_stack(st_a, st_b);
+}
